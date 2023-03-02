ac27755b2f85da32a59e19c0a0d9a1e09877cdd3
MigeulEliotte <97182631+MigeulEliotte@users.noreply.github.com>
2023-03-02 13:58:21 -0500

    Create translate.cpp

---
+++ Exams/Exam01/translate.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+
+int main() {
+
+string callsign[26] = {"ALFA", "CHARLIE" , "ECHO", "GOLF","INDIA","","KILO","MIKE","OSCAR","QUEBEC","SIERRA","UNIFORM","WHISKEY","YANKEE"} ;
+
+cout<< callsign[1];
+  return 0;
+}
